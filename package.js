493a717a09c86ca7f5f460a41448371404cfea6a
Zack Newsham <zack_newsham@yahoo.co.uk>
2018-04-30 23:08:23 -0400

    fix autoForm

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: "znewsham:dynamic-table",
   summary: "Flexible datatables for large collections in Meteor",
-  version: "0.1.1",
+  version: "0.1.2",
   git: "https://bitbucket.org/znewsham/meteor-dynamic-tables"
 });
 
