0708ac92ed8e9cf7cdc30909e16891f187110734
Zack Newsham <zack_newsham@yahoo.co.uk>
2018-07-16 22:49:53 -0400

    fix quote export

--- client/ui/exportModal.js
+++ client/ui/exportModal.js
@@ -82,7 +82,7 @@ function CSVLineFromDocument(doc, exportOptions, columns, fieldNames, selector) 
   .map((val) => {
     // NOTE: if the resulting value contains commas, we need to escape any present quotes and then quote the entire field
     if (val.indexOf(",") !== -1) {
-      val = val.split("\"").join("\\\"");
+      val = val.split("\"").join("\"\"");
       val = `"${val}"`;
     }
     return val;
