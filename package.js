e2914a82b3f96b6385899b0b697e5f77a59f2422
Zack Newsham <zack_newsham@yahoo.co.uk>
2019-02-24 15:56:12 -0500

    make lazy

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: "znewsham:dynamic-table",
   summary: "Flexible datatables for large collections in Meteor",
-  version: "1.0.4",
+  version: "1.0.5",
   git: "https://bitbucket.org/znewsham/meteor-dynamic-tables"
 });
 
@@ -34,8 +34,8 @@ Package.onUse((api) => {
   // before any tabular tables are rendered
   api.use(["jquery"], "client", { weak: true });
 
-  api.mainModule("server/main.js", "server", { lazy: true });
-  api.mainModule("client/main.js", "client");
+  api.mainModule("server/main.js", "server");
+  api.mainModule("client/main.js", "client", { lazy: true });
 });
 
 Package.onTest((api) => {
