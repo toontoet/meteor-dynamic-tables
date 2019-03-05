145feb45125ff0b94c17a3f45a9664d906182973
Zack Newsham <zack_newsham@yahoo.co.uk>
2019-03-05 10:24:15 -0500

    bulk edit reactivity

--- client/ui/components/bulkEditForm/bulkEditForm.js
+++ client/ui/components/bulkEditForm/bulkEditForm.js
@@ -138,6 +138,7 @@ Template.dynamicTableBulkEditForm.onCreated(function onCreated() {
 
   const self = this;
   this.autorun(() => {
+    const tableData = Template.currentData().tableData;
     const additionalCols = self.additionalCols.get();
     const subsFields = additionalCols.map(f => ({ [f]: 1 })).reduce((acc, val) => Object.assign(acc, val), {});
     const handle = self.subscribe(tableData.table.publication, { _id: { $in: documentIds } }, { /* fields: subsFields */ });
@@ -180,93 +181,6 @@ Template.dynamicTableBulkEditForm.helpers({
 });
 
 Template.dynamicTableBulkEditForm.events({
-  "click #cancel"(e) {
-    e.preventDefault();
-    $("#bulk-edit-modal").modal("hide");
-  },
-  "click #update"(e) {
-    e.preventDefault();
-    let fields = Template.instance().fields.get();
-    const collection = Template.instance().collection;
-    const bulkEditOptions = Template.instance().bulkEditOptions;
-    const documentIds = Template.currentData().documentIds;
-
-    fields = fields.filter(field => field.bulkEditDisplay);
-
-    const updatedEntries = [];
-    const skippedEntries = [];
-    const failedEntries = [];
-    const documentsToUpdate = collection.find({ _id: { $in: documentIds } }, { fields: { _id: true } });
-    Promise.all(documentsToUpdate.map(doc => new Promise(((resolve, reject) => {
-      try {
-        fields.forEach((field) => {
-          const editRowData = {
-            doc,
-            column: field,
-            collection
-          };
-          const editTemplateData = field.editTmplContext ? field.editTmplContext(editRowData) : editRowData;
-          const fieldSelector = $(document.getElementById(`${field.data}-input`));
-          let fieldValue = fieldSelector.val();
-          const extra = fieldSelector.data("select2") ? fieldSelector.data("select2").data() : undefined;
-
-          if (fieldValue && _.isArray(fieldValue) && fieldValue.length) {
-            fieldValue = fieldValue.filter(v => !!v);
-          }
-
-          if (editTemplateData.editCallback) {
-            const placeholder = fieldSelector.data("select2") ? fieldSelector.data("select2").results.placeholder.text : fieldSelector.attr("placeholder");
-            if (!fieldValue || (!fieldValue.length && placeholder === "Multiple Values")) {
-              // Skip (Field has multiple initial values and no new entries added by user)
-              skippedEntries.push({ _id: doc._id, field: field.title });
-            }
-            else {
-              editTemplateData.editCallback(doc._id, fieldValue, doc, () => {
-                // Handle success
-                updatedEntries.push({ _id: doc._id, field: field.title });
-              }, extra, true);
-            }
-          }
-          else {
-            const placeholder = fieldSelector.attr("placeholder");
-            if (!fieldValue && placeholder === "Multiple Values") {
-              // Skip (Field has multiple initial values and no new entries added by user)
-              skippedEntries.push({ _id: doc._id, field: field.title });
-            }
-            else {
-              const $set = {};
-              $set[field.data] = fieldValue;
-              collection.update({ _id: doc._id }, { $set }, (err, res) => {
-                if (err) {
-                  // Handle error
-                  failedEntries.push({ _id: doc._id, field: field.title, err });
-                }
-                else {
-                  // Handle success
-                  updatedEntries.push({ _id: doc._id, field: field.title });
-                }
-              });
-            }
-          }
-        });
-        resolve();
-      }
-      catch (error) {
-        reject(error);
-      }
-    }))))
-    .then(() => {
-      if (bulkEditOptions && typeof bulkEditOptions.onSuccess === "function") {
-        bulkEditOptions.onSuccess(updatedEntries, skippedEntries, failedEntries);
-      }
-    })
-    .catch((err) => {
-      if (bulkEditOptions && typeof bulkEditOptions.onError === "function") {
-        bulkEditOptions.onError(err);
-      }
-    });
-    $("#bulk-edit-modal").modal("hide");
-  },
   "click #add-editable-column"(e) {
     const editableColId = $("#add-editable-column-id").val();
     const additionalCols = Template.instance().additionalCols.get();
