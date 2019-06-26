a031f11e4340f2915eabb68e6c66731a7492c4ec
Zack Newsham <zack_newsham@yahoo.co.uk>
2019-06-26 16:20:06 -0400

    hacky fix for when multiple jquery versions are in use

--- client/main.js
+++ client/main.js
@@ -9,3 +9,8 @@ export { inlineSave, getValue, nextField } from "./inlineSave.js";
 if (!$.fn.DataTable) {
   dataTableInit(window, $);
 }
+
+// HACK: to handle npm blaze and the mess that is meteor vs npm jquery
+if (!Blaze._DOMBackend._$jq.fn.DataTable) {
+  dataTableInit(window, Blaze._DOMBackend._$jq);
+}
