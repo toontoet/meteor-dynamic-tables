82f1d10e27b2b3b0692a1718933505eea4a68854
Tim Nimets <tnimets@justplayss.com>
2019-09-25 11:03:10 -0400

    JUS-158 #comment reformated into Blaze from Blaze-component. added more flexible date formating. Fixed apelling mistake

--- client/ui/components/dateValueEditor/dateValueEditor.js
+++ client/ui/components/dateValueEditor/dateValueEditor.js
@@ -2,91 +2,74 @@
 import "./dateValueEditor.html";
 import "./dateValueEditor.css";
 
-import { BlazeComponent } from "meteor/znewsham:blaze-component";
 import { inlineSave } from "../../../inlineSave.js";
 
-export class dynamicTableDateValueEditor extends BlazeComponent {
-  constructor(templInstance) {
-     super(templInstance);
+Template.dynamicTableDateValueEditor.onRendered(function() {
+  const input = this.$("input.form-control");
+  const isMultiple = this.data.multiple;
+  let datePickerFn = input.datepicker ? input.datepicker.bind(input) : () => {};
+  if (this.data.options.datePickerFn) {
+    datePickerFn = this.data.options.datePickerFn.bind(input);
+  }
+  else if (isMultiple || this.data.options.useBootstrap) {
+    // if bootstrap datepicker is not imported it'll keep using jquery datepicker
+    try {
+      const oldDatePicker = $.fn.datepicker;
+      const bsDatePicker = require("bootstrap-datepicker");
+      if ($.fn.datepicker) {
+        datePickerFn = $.fn.datepicker.bind(input);
+      }
+      // $.fn.datepicker = oldDatePicker;
+    }
+    catch (e) { 
+      console.warn("Bootstrap datepicker is not found. The program will use the builtin/available datepicker insted. Some functionality limmitaion are expected");
+    }
   }
 
-  rendered() {
-    const input = this.$("input.form-control");
-    const isMultiple = this.reactiveData().multiple;
-    let datePickerFn = input.datepicker ? input.datepicker.bind(input) : () => {};
-    if (this.reactiveData().options.datePickerFn) {
-      datePickerFn = this.reactiveData().options.datePickerFn.bind(input);
+  const defaultDateFormat = "dd/mm/yyyy"
+  const dateFormat = this.data.options.dateFormat || defaultDateFormat
+  // if bootstrap datepicker is not imported into the project 
+  // it will use jquery datepicker syntax
+  const datePickerRet = datePickerFn({
+    multidate: isMultiple,// will be ignored for jquery
+    format: dateFormat,// will be ignored for jquery
+    onClose: function(date, datepicker) {
+      input.trigger("needToUpdate");
     }
-    else if (isMultiple || this.reactiveData().options.useBootstrap) {
-      // if bootstrap datepicker is not imported it'll keep using jquery datepicker
-      try {
-        const oldDatePicker = $.fn.datepicker;
-        const bsDatePicker = require("bootstrap-datepicker");
-        if ($.fn.datepicker) {
-          datePickerFn = $.fn.datepicker.bind(input);
-        }
-        // $.fn.datepicker = oldDatePicker;
+  });
+  // if bootstrap date picker was imported 
+  if (datePickerRet.on) {
+    datePickerRet.on("changeDate", function(e) {
+      if (! isMultiple) {
+        input.datepicker("hide");
       }
-      catch (e) { 
-        console.warn("Bootstrap datepicker is not found. The program will use the builtin/aviable datepicker insted. Some functionality limmitaion are expected");
-      }
-    }
-    // if bootstrap datepicker is not imported into the project 
-    // it will use jquery datepicker syntax
-    const datePickerRet = datePickerFn({
-      multidate: isMultiple,// will be ignored for jquery
-      formate: "dd/mm/yyyy",// will be ignored for jquery
-      onClose: function(date, datepicker) {
-        input.trigger("needToUpdate");
-      }
+    }).on("hide", function(e){
+      input.trigger("needToUpdate");
     });
-    // if bootstrap date picker was imported 
-    if (datePickerRet.on) {
-      datePickerRet.on("changeDate", function(e) {
-        if (! isMultiple) {
-          input.datepicker("hide");
-        }
-      }).on("hide", function(e){
-        input.trigger("needToUpdate");
-      });
-    }
-    $(":focus").focus(); // to trigger datepicker appear
   }
+  $(":focus").focus(); // to trigger datepicker appear
+});
 
-  destructor() {
-    // disables any timeouts/intervals associated with this component instance
-    super.destructor();
+Template.dynamicTableDateValueEditor.helpers({
+  date() {
+    return Template.instance().data.value;
   }
+});
 
-  static HelperMap() {
-    return {
-      date: "date"
-    }
-  }
-
-  static EventMap() {
-    return {
-      "needToUpdate input.form-control": "onNeedToUpdate"
-    }
-  }
-
-  onNeedToUpdate(e, templInstance, ...args) {
-    const input = $(e.target)
+Template.dynamicTableDateValueEditor.events({
+  "needToUpdate input.form-control"(e, templInstance, ...args){
+    const input = $(e.target);
+    const defaultMomentDateFormat = "DD/MM/YY";
+    const momentDateFormat = Template.instance().data.options.dateFormat ? Template.instance().options.data.dateFormat.toUpperCase().replace("YYYY", "YY") : defaultMomentDateFormat;
     // moment validation AND regex 2 digit/2 digit/4 digit OR empty input so date can be erased 
-    if (moment(input.val(), "MM/DD/YY").isValid() && /^(\d{2}\/\d{2}\/\d{4},?)+$/.test(input.val()) || input.val() === "") {
+    if (moment(input.val(), momentDateFormat).isValid() && /^(\d{2}\/\d{2}\/\d{4},?)+$/.test(input.val()) || input.val() === "") {
       input.removeClass("date-invalid");
       if (templInstance.data.saveOnEnter !== false) {
-        const newDateValue = this.reactiveData().multiple ? input.val().split(',').filter((date) => moment(date, "MM/DD/YY").isValid()): input.val();
+        const newDateValue = Template.instance().data.multiple ? input.val().split(',').filter((date) => moment(date, momentDateFormat).isValid()): input.val();
         inlineSave(templInstance, newDateValue);
       }
     } else {
       input.addClass("date-invalid");
     }
   }
-
-  date() {
-    return this.nonReactiveData().value;
-  }
-}
-
-BlazeComponent.register(Template.dynamicTableDateValueEditor, dynamicTableDateValueEditor);
+});
